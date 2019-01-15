0956dfb5f5beb3c77bb43775a02d59864c728236
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-01-15 03:22:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function printBadges(arg) {
+  for (let i = 0; i < arg.length; i++) {
+    console.log(`Welcome ${arg[i]}! You are employee #${i + 1}.`);
+  }
+  return arg;
+};
+
+
+ function tailsNeverFails() {
+   i = 0;
+   while (Math.random() >= 0.5){
+     i++
+   } return `You got ${i} tails in a row!`
+ }
