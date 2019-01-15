781349911fb85213de50de847e4026e0874fa756
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-01-15 03:20:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function printBadges(arg) {
-  for (i = 0, i < arg.length, i++) {
+  for (let i = 0; i < arg.length; i++) {
     console.log(`Welcome ${arg[i]}! You are employee #${i}.`);
   }
   return arg;
